5c3c766e298d57f0258e01200834ed3f1b47ad51
Daniel <daniel.johns@robotsandpencils.com>
2023-01-21 22:16:53 -0800

    Day 4 Part 1

--- Day4/Day4.swift
+++ Day4/Day4.swift
@@ -6,9 +6,59 @@
 //
 
 import Foundation
+import RegexBuilder
 
 final class Day4: Day {
     func run(input: String) -> String {
-        return ""
+        let name = Reference(Substring.self)
+        let id = Reference(Int.self)
+        let checksum = Reference(Substring.self)
+        let regex = Regex {
+            Capture(as: name) {
+                OneOrMore(ChoiceOf {
+                    .word
+                    "-"
+                })
+            } transform: {
+                $0.replacing("-", with: "")
+            }
+            "-"
+            TryCapture(as: id) {
+                OneOrMore(.digit)
+            } transform: {
+                Int($0)
+            }
+            "["
+            Capture(as: checksum) { OneOrMore(.word) }
+            "]"
+        }
+        
+        return input.lines
+            .map { $0.wholeMatch(of: regex)! }
+            .filter { String(sorted(byLetterFrequency: $0[name]).prefix(5)) == $0[checksum] }
+            .map { $0[id] }
+            .sum
+            .description
+    }
+    
+    func sorted(byLetterFrequency string: Substring) -> [Character] {
+        letterCounts(of: string)
+            .sorted {
+                if $0.value == $1.value {
+                    return $0.key < $1.key
+                } else {
+                    return $0.value > $1.value
+                }
+            }
+            .map { $0.key }
+    }
+    
+    func letterCounts(of string: Substring) -> [Character: Int] {
+        var totals = [Character: Int]()
+        for character in string {
+            totals[character, default: 0] += 1
+        }
+        
+        return totals
     }
 }
--- Day4/Day4Tests.swift
+++ Day4/Day4Tests.swift
@@ -13,7 +13,11 @@ final class Day4Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "1514")
     }
 }
