7f636d0e3d7e28b5ea54831f6c6e19878afa9cd7
Daniel <daniel.johns@robotsandpencils.com>
2023-01-22 15:34:22 -0800

    Day 8 Part 1

--- Day8/Day8.swift
+++ Day8/Day8.swift
@@ -6,9 +6,52 @@
 //
 
 import Foundation
+import RegexBuilder
 
 final class Day8: Day {
     func run(input: String) -> String {
-        return ""
+        let rect = Regex {
+            "rect "
+            TryCapture(OneOrMore(.digit)) { Int($0) }
+            "x"
+            TryCapture(OneOrMore(.digit)) { Int($0) }
+        }
+        
+        let rotateRow = Regex {
+            "rotate row y="
+            TryCapture(OneOrMore(.digit)) { Int($0) }
+            " by "
+            TryCapture(OneOrMore(.digit)) { Int($0) }
+        }
+        
+        let rotateColumn = Regex {
+            "rotate column x="
+            TryCapture(OneOrMore(.digit)) { Int($0) }
+            " by "
+            TryCapture(OneOrMore(.digit)) { Int($0) }
+        }
+        
+        var lights = Set<Point>()
+        for line in input.lines {
+            if let match = line.wholeMatch(of: rect)?.output {
+                for x in 0 ..< match.1 {
+                    for y in 0 ..< match.2 {
+                        lights.insert([x, y])
+                    }
+                }
+            } else if let match = line.wholeMatch(of: rotateRow)?.output {
+                var row = lights.filter { $0.y == match.1 }
+                lights.subtract(row)
+                lights.formUnion(row.map { Point(x: ($0.x + match.2) % 50, y: $0.y)})
+            } else if let match = line.wholeMatch(of: rotateColumn)?.output {
+                var column = lights.filter { $0.x == match.1 }
+                lights.subtract(column)
+                lights.formUnion(column.map { Point(x: $0.x, y: ($0.y + match.2) % 6)})
+            } else {
+                fatalError("Unknown instruction: \(line)")
+            }
+        }
+        
+        return lights.count.description
     }
 }
