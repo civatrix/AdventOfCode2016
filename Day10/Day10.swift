6a65f710f7290a4b705170c981b354a6774ab50f
Daniel <daniel.johns@robotsandpencils.com>
2023-01-23 19:18:08 -0800

    Day 10 Part 1

--- Day10/Day10.swift
+++ Day10/Day10.swift
@@ -8,7 +8,52 @@
 import Foundation
 
 final class Day10: Day {
+    enum Target {
+        case bot(Substring)
+        case output(Substring)
+    }
+    
     func run(input: String) -> String {
-        return ""
+        var bots = [Substring: [Int]]()
+        var output = [Substring: [Int]]()
+        var instructions = [Substring: (low: Target, high: Target)]()
+        
+        for line in input.lines {
+            if let match = line.wholeMatch(of: /value (\d+) goes to bot (\d+)/)?.output {
+                bots[match.2, default: []].append(Int(match.1)!)
+            } else if let match = line.wholeMatch(of: /bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)/)?.output {
+                let low = match.2 == "bot" ? Target.bot(match.3) : Target.output(match.3)
+                let high = match.4 == "bot" ? Target.bot(match.5) : Target.output(match.5)
+                instructions[match.1] = (low: low, high: high)
+            } else {
+                fatalError("Unknown instruction: \(line)")
+            }
+        }
+        
+        assert(bots.first { $0.value.count > 2 } == nil)
+    
+        while let bot = bots.first(where: { $0.value.count == 2 }) {
+            let chips = bots.removeValue(forKey: bot.key)!
+            let instruction = instructions[bot.key]!
+            
+            let low = chips.min()!
+            let high = chips.max()!
+            
+            if low == 17 && high == 61 {
+                return String(bot.key)
+            }
+            
+            switch instruction.low {
+            case let .output(key): output[key, default: []].append(low)
+            case let .bot(key): bots[key, default: []].append(low)
+            }
+            
+            switch instruction.high {
+            case let .output(key): output[key, default: []].append(high)
+            case let .bot(key): bots[key, default: []].append(high)
+            }
+        }
+        
+        return "not found"
     }
 }
--- Day10/Day10Tests.swift
+++ Day10/Day10Tests.swift
@@ -13,7 +13,13 @@ final class Day10Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+value 5 goes to bot 2
+bot 2 gives low to bot 1 and high to bot 0
+value 3 goes to bot 1
+bot 1 gives low to output 1 and high to bot 0
+bot 0 gives low to output 2 and high to output 0
+value 2 goes to bot 2
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "2")
     }
 }
