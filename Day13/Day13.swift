9eee4e8cf8ca90bcb5fc68bbf3806513c41fcd44
Daniel <daniel.johns@robotsandpencils.com>
2023-01-24 21:08:17 -0800

    Day 13 Part 2

--- Day13/Day13.swift
+++ Day13/Day13.swift
@@ -8,31 +8,25 @@
 import Foundation
 
 final class Day13: Day {
-    struct InfiniteGridGraph: Graph {
-        struct Edge: WeightedEdge {
-            let cost = 1
-            let target: Point
-        }
-        
-        let salt: Int
-        
-        func edgesOutgoing(from vertex: Point) -> [Edge] {
-            return [
-                vertex + .up,
-                vertex + .down,
-                vertex + .left,
-                vertex + .right,
-            ].filter { $0.isOpen(salt) }
-                .map(Edge.init(target:))
-        }
-    }
-    
     func run(input: String) -> String {
         let salt = Int(input.trimmingCharacters(in: .newlines))!
-        let aStar = AStar(graph: InfiniteGridGraph(salt: salt), heuristic: Point.distance(between:and:))
+    
+        var visited = Set<Point>([[1,1]])
+        var newlyVisited = Set<Point>([[1,1]])
+        var step = 0
+        while step < 50 {
+            newlyVisited = Set(newlyVisited.flatMap {
+                $0.adjacent
+                    .filter { $0.x >= 0 && $0.y >= 0 }
+                    .filter { $0.isOpen(salt) }
+            }).subtracting(visited)
+            visited.formUnion(newlyVisited)
+            step += 1
+        }
         
-        return (aStar.path(start: [1,1], target: [31,39]).count - 1).description
+        return visited.count.description
     }
+    
 }
 
 private extension Point {
