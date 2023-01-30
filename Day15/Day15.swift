fd3803a033ee5acad069dfcd4e7bd8b392c1e5fa
Daniel <daniel.johns@robotsandpencils.com>
2023-01-29 22:26:09 -0800

    Day 15 Part 1

--- Day15/Day15.swift
+++ Day15/Day15.swift
@@ -6,9 +6,31 @@
 //
 
 import Foundation
+import RegexBuilder
 
 final class Day15: Day {
+    let regex = Regex {
+        "Disc #"
+        One(.digit)
+        " has "
+        TryCapture(OneOrMore(.digit)) { Int($0) }
+        " positions; at time=0, it is at position "
+        TryCapture(OneOrMore(.digit)) { Int($0) }
+        "."
+    }
     func run(input: String) -> String {
-        return ""
+        let discs = input.lines.map {
+            let match = $0.wholeMatch(of: regex)!
+            return (size: match.1, start: match.2)
+        }
+        
+        var time = 0
+        while true {
+            if discs.enumerated().allSatisfy({ (time + $0.offset + 1 + $0.element.start) % $0.element.size == 0 }) {
+                return time.description
+            }
+            
+            time += 1
+        }
     }
 }
--- Day15/Day15Tests.swift
+++ Day15/Day15Tests.swift
@@ -13,7 +13,9 @@ final class Day15Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "5")
     }
 }
