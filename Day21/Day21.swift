9aac95b9e206e7183c59b5a9f22dd99885d156cf
Daniel <daniel.johns@robotsandpencils.com>
2023-03-01 21:00:33 -0800

    Day 21 Part 1

--- Day21/Day21.swift
+++ Day21/Day21.swift
@@ -8,7 +8,55 @@
 import Foundation
 
 final class Day21: Day {
+    var startingPassword = "abcdefgh"
+    
+    let swapPosition = /swap position (.) with position (.)/
+    let swapLetter = /swap letter (.) with letter (.)/
+    let rotate = /rotate (left|right) (.) steps?/
+    let rotateLetter = /rotate based on position of letter (.)/
+    let reverse = /reverse positions (.) through (.)/
+    let move = /move position (.) to position (.)/
+    
     func run(input: String) -> String {
-        return ""
+        var password = startingPassword.map { $0 }
+        for line in input.lines {
+            if let match = line.wholeMatch(of: swapPosition)?.output {
+                let from = Int(match.1)!
+                let to = Int(match.2)!
+                password.swapAt(from, to)
+            } else if let match = line.wholeMatch(of: swapLetter)?.output {
+                let from = password.firstIndex(of: Character(String(match.1)))!
+                let to = password.firstIndex(of: Character(String(match.2)))!
+                password.swapAt(from, to)
+            } else if let match = line.wholeMatch(of: rotate)?.output {
+                let steps = Int(match.2)!
+                let newStart: Int
+                if match.1 == "left" {
+                    newStart = steps
+                } else {
+                    newStart = (password.count - steps) % password.count
+                }
+                password.rotate(toStartAt: newStart)
+            } else if let match = line.wholeMatch(of: rotateLetter)?.output {
+                var from = password.firstIndex(of: Character(String(match.1)))! + 1
+                if from > 4 {
+                    from += 1
+                }
+                let newStart = password.count - (from % password.count)
+                password.rotate(toStartAt: newStart)
+            } else if let match = line.wholeMatch(of: reverse)?.output {
+                let from = Int(match.1)!
+                let to = Int(match.2)!
+                password.reverse(subrange: from ..< to + 1)
+            } else if let match = line.wholeMatch(of: move)?.output {
+                let from = Int(match.1)!
+                let to = Int(match.2)!
+                password.insert(password.remove(at: from), at: to)
+            } else {
+                fatalError("Invalid instruction: \(line)")
+            }
+        }
+        
+        return String(password)
     }
 }
--- Day21/Day21Tests.swift
+++ Day21/Day21Tests.swift
@@ -10,10 +10,91 @@ import XCTest
 final class Day21Tests: XCTestCase {
     let day = Day21()
     
-    func testDay() throws {
+    func testDayFull() throws {
+        day.startingPassword = "abcde"
         let input =
 """
+swap position 4 with position 0
+swap letter d with letter b
+reverse positions 0 through 4
+rotate left 1 step
+move position 1 to position 4
+move position 3 to position 0
+rotate based on position of letter b
+rotate based on position of letter d
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "decab")
+    }
+    
+    func testDay1() throws {
+        day.startingPassword = "abcde"
+        let input =
+"""
+swap position 4 with position 0
+"""
+        XCTAssertEqual(day.run(input: input), "ebcda")
+    }
+    
+    func testDay2() throws {
+        day.startingPassword = "ebcda"
+        let input =
+"""
+swap letter d with letter b
+"""
+        XCTAssertEqual(day.run(input: input), "edcba")
+    }
+    
+    func testDay3() throws {
+        day.startingPassword = "edcba"
+        let input =
+"""
+reverse positions 0 through 4
+"""
+        XCTAssertEqual(day.run(input: input), "abcde")
+    }
+    
+    func testDay4() throws {
+        day.startingPassword = "abcde"
+        let input =
+"""
+rotate left 1 step
+"""
+        XCTAssertEqual(day.run(input: input), "bcdea")
+    }
+    
+    func testDay5() throws {
+        day.startingPassword = "bcdea"
+        let input =
+"""
+move position 1 to position 4
+"""
+        XCTAssertEqual(day.run(input: input), "bdeac")
+    }
+    
+    func testDay6() throws {
+        day.startingPassword = "bdeac"
+        let input =
+"""
+move position 3 to position 0
+"""
+        XCTAssertEqual(day.run(input: input), "abdec")
+    }
+    
+    func testDay7() throws {
+        day.startingPassword = "abdec"
+        let input =
+"""
+rotate based on position of letter b
+"""
+        XCTAssertEqual(day.run(input: input), "ecabd")
+    }
+    
+    func testDay8() throws {
+        day.startingPassword = "ecabd"
+        let input =
+"""
+rotate based on position of letter d
+"""
+        XCTAssertEqual(day.run(input: input), "decab")
     }
 }
